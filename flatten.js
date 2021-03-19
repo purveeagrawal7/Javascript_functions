96a782664e9bc8ce7b43a5350324740d848059e7
Purvee Agrawal <purvee.agrawal@gmail.com>
2021-03-19 12:02:07 +0530

    Update flatten.js
    Example:,
    INPUT - flatten([1, [2, 3], [[4], [5]])
    OUTPUT - [ 1, 2, 3, 4, 5 ]

--- flatten.js
+++ flatten.js
@@ -1,7 +1,5 @@
-/* Write a Program to Flatten a given n-dimensional array */
 let finalArray = [];
 const flatten = (arr) => {
-	// Write your code here
 	if(!(Array.isArray(arr))) return null;
 	for( let i = 0; i< arr.length; i++) {
 		if(Array.isArray(arr[i])) {
@@ -13,11 +11,4 @@ const flatten = (arr) => {
 	}
 	return finalArray;
 };
-
-/* For example,
-INPUT - flatten([1, [2, 3], [[4], [5]])
-OUTPUT - [ 1, 2, 3, 4, 5 ]
-
-*/
-
 module.exports = flatten;
